e3250b5098de50815a20667c690e73bc2d952054
Vinay Kumar <vnykumar9632@gmail.com>
2025-07-10 23:55:58 +0530

    all the work og day01

---
+++ terraform730am/day03-all-configfiles/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "name" {
+    ami = "ami-05ffe3c48a9991133"
+    instance_type = "t2.micro"
+    key_name = "testdevprod"
+    tags = { 
+   Name = "day-3"}
+  
+}
---
+++ terraform730am/day03-all-configfiles/output.tf
@@ -0,0 +1,4 @@
+output "public_ip" {
+    value = aws_instance.name.public_ip
+  
+}
